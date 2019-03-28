5191200d5a9efa75700579569c8c33b56917933d
takasy <ticktack016@gmail.com>
2019-03-28 10:56:43 +0900

    01.2 Part A: Your first interactive UI

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun countUp(view: View) {
+        ++count
+        showCount.text = count.toString()
     }
 }
